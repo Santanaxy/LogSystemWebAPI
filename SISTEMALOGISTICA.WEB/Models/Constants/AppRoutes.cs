6d1619b3e98f3842138177c218d51ed05d9d29a3
victor.rezende <victor.rezende@plural.com.br>
2026-07-14 16:53:21 -0300

    Conseguir fazer algumas alterações que vou da continuidade no meu projeto de desenvolver sistema de logistica com os componentes em mudblazor

--- SISTEMALOGISTICA.WEB/Models/Constants/AppRoutes.cs
+++ SISTEMALOGISTICA.WEB/Models/Constants/AppRoutes.cs
@@ -3,16 +3,10 @@
     public class AppRoutes
     {
         public const string HOME = "/";
-        public const string CLIENTES = "/clientes";
-        public const string SERVICOS = "/servicos";
-        public const string ORDENS_SERVICO = "/ordens-servico";
-        public const string ORCAMENTOS = "/orcamentos";
-        public const string CALENDARIO_POST = "/calendario-postagem";
-        public const string PLANEJAMENTO_CONTEUDO = "/listagem-planej-cont";
-        public const string LISTAGEM_POSTS = "/listagem-posts";
-        public const string LISTAGEM_TEMPLATES = "/templates";
-        public const string LISTAGEM_DOCUMENTOS = "/documentos";
-        public const string QUADRO_DEMANDAS = "/workflow";
+        public const string CADASTRO = "/cadastroFuncionario";
+        public const string DASHBOARD = "/dashboardfuncionario";
+         
+
 
     }
 }
